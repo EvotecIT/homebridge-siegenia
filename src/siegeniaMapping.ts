ded7a0c8c7f08da79b003e623f94ed0a54ec2f9b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-24 23:34:45 +0100

    Add basic mapping

---
+++ src/siegeniaMapping.ts
@@ -0,0 +1,13 @@
+﻿// List of defined device types
+export const DeviceTypeMap = {
+    1: 'AEROPAC',
+    2: 'AEROMAT VT',
+    3: 'DRIVE axxent Family',
+    4: 'SENSOAIR',
+    5: 'AEROVITAL',
+    6: 'MHS Family',
+    7: 'reserved',
+    8: 'AEROTUBE',
+    9: 'GENIUS B', // (obsolete – GENIUS B is now controlled by the Universal Module)
+    10: 'Universal Module'
+};
